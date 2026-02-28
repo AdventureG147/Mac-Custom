0cc700527daebdcc8fccac3d706aeec3704291b0
AdventureG147 <bsbalash99@gmail.com>
2026-02-28 16:24:28 +0300

    Added two config lines to wezterm.lua (disabled one of them)

--- wezterm.lua
+++ wezterm.lua
@@ -10,12 +10,15 @@ config.initial_rows = 28
 
 -- Appearance
 config.font_size = 15
+config.font = wezterm.font("Hack Nerd Font")
 config.color_scheme = 'Neon Night (Gogh)'
 config.colors = {
     cursor_border = '#769ff0',
     cursor_bg = '#769ff0',
     cursor_fg = '#769ff0',
 }
+-- config.window_decorations = 'RESIZE' --Removes window bar and allows you to resize the window without it.
+config.enable_tab_bar = false
 
 -- Finally, return the configuration to wezterm:
 return config
