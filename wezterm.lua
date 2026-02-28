7bdb56451074c88b832d17f08bb6fb430f46fcc7
AdventureG147 <bsbalash99@gmail.com>
2026-02-28 15:20:52 +0300

    More changes to wezterm.lua (Changed cursor color)

--- wezterm.lua
+++ wezterm.lua
@@ -10,8 +10,12 @@ config.initial_rows = 28
 
 -- Appearance
 config.font_size = 15
-
 config.color_scheme = 'Neon Night (Gogh)'
+config.colors = {
+    cursor_border = '#769ff0',
+    cursor_bg = '#769ff0',
+    cursor_fg = '#769ff0',
+}
 
 -- Finally, return the configuration to wezterm:
 return config
